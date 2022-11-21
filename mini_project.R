9cc39b251fe2e64baa3c0c07be73dfa26955912d
MagaliCorti <92572856+MagaliCorti@users.noreply.github.com>
2022-11-21 19:12:22 +0100

    Update mini_project.R

--- mini_project.R
+++ mini_project.R
@@ -54,35 +54,23 @@ Coef_P <- CTM %>%
 CTM <- cbind(CTM, Coef_P[,2])
 
 # selecting PAR at ~50cm
-PAR50 <- CTM %>%
-  group_by(Replicate) %>%
-  mutate(Coef_P = ifelse(depth=0.50,paste0(Coef_P),paste0(Coef_P)))
+PAR50 <- CTM$depth >= 0.49 & CTM$depth <= 0.51
+PAR50sub <- subset(CTM, PAR50)
+CoefM <- PAR50sub %>%
+  group_by(Name) %>%
+  summarize(Coef_P=mean(Coef_P))
+
+CTM <- merge(CTM, Means, by="Name")
+CTM <- merge(CTM, CoefM, by="Name")
+CTM <- CTM[,-c(6:13)]
   
-  
-PAR50 <- CTM %>%
-  group_by(Replicate) %>%
-  mutate(Coef_P = if(depth=0.50,paste0(Coef_P))
+CTM <- unique(CTM)
 
-
-for(i in 1:nrow(cluster_2001)){
-  if(rowSums(cluster_2001[i,c(4:11)])>0){
-    cluster_2001$community[i]=cluster_2001$comm[i]
-  }else
-    cluster_2001$community[i]=NA
-}
-
-
-  cluster_2001= cluster_2001%>% 
-  mutate(ligne = ifelse(li<10,paste0("L0",li),paste0("L",li)),
-         colone = ifelse(col<10,paste0("C0",col),paste0("C",col)),
-         Site = paste0(ligne,colone),.keep="unused") %>%
-  dplyr::select(-c(ligne, colone, Plot)) %>% 
-  rename(Plot=Site)
-
-
-
-
-
+rm(PAR50)
+rm(PAR50sub)
+rm(Means)
+rm(Coef_P)
+rm(CoefM)
 
 
 
@@ -93,7 +81,6 @@ ggplot(pigments, aes(y=Chla, x=Sampling, fill=Treatment)) +
 
 ggplot(pigments, aes(y=Carot, x=Sampling, fill=Treatment)) +
   geom_boxplot()
-
 
 
 
